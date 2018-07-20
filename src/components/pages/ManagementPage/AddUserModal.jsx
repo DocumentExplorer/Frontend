79b75441fc98a97fd3f5df1749c526963d86f4f9
javaxd123 <marcinwarzybok@outlook.com>
2018-07-20 12:53:07 +0200

    add new user working

--- src/components/pages/ManagementPage/AddUserModal.jsx
+++ src/components/pages/ManagementPage/AddUserModal.jsx
@@ -3,15 +3,15 @@ import { Input, Fa } from 'mdbreact'
 import Select from 'react-select-me'
 
 const options = [
-    { value: 'Admin', label: 'Admin' },
-    { value: 'User', label: 'User' },
+    { value: 'admin', label: 'Admin' },
+    { value: 'user', label: 'User' },
 ];
 
 export const AddUserModal = ({ handleChange, handleSelect, role, ...props }) => {
     return (
         <Fragment>
             <Input name="username" label="Nazwa Użytkownika" icon="user" onChange={(e) => handleChange(e)} />
-            <Input name="password" label="Hasło" icon="lock" onChange={(e) => handleChange(e)} />
+            <Input name="password" label="Hasło" icon="lock" type="password" onChange={(e) => handleChange(e)} />
             <div className="account-type">
                 <label>Typ konta</label>
                 <Select
--- src/redux/services/register.service.js
+++ src/redux/services/register.service.js
@@ -1,16 +1,16 @@
 import axios from 'axios'
 import { ApiConstants } from '../constants'
 import _ from 'lodash'
+import { getToken } from '../../components/helpers/getToken';
 
 function register(values) {
-    console.log(values)
     return new Promise((resolve, reject) => {
         axios({
-            url: `${ApiConstants}/users/register`,
-            data: _.pick(values, ['username', 'password', 'role']),
+            url: `${ApiConstants.rootURL}/users/register`,
             method: 'POST',
+            data: values,
             headers: {
-                'Authorization': localStorage.getItem('token'),
+                'Authorization': getToken(),
                 'Content-Type': 'application/json'
             }
         }).then((res) => {
