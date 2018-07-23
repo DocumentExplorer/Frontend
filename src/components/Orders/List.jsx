0da3f949bf6178fe0428c213e6311cfd3b5f82b5
javaxd123 <marcinwarzybok@outlook.com>
2018-07-23 12:15:37 +0200

    remove bugs from dashboard pag

--- src/components/Orders/List.jsx
+++ src/components/Orders/List.jsx
@@ -22,7 +22,7 @@ class List extends React.Component {
 
     render() {
         const { data } = this.props
-        const maxLenth = new Date().getFullYear() - 2005
+        const maxLenth = new Date().getFullYear() - 2015
         if (this.state.years.length <= maxLenth) {
             for (let i = 2015; i <= new Date().getFullYear(); i++) {
                 this.state.years.push(new OrderByYear(i, _.filter(data, (item) => {
