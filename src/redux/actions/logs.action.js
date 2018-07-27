ec2e56a42648f040263b6ee606919b33fdc8a67b
javaxd123 <marcinwarzybok@outlook.com>
2018-07-27 10:30:55 +0200

    add constants for logs

---
+++ src/redux/actions/logs.action.js
@@ -0,0 +1,31 @@
+export function getLogs() {
+    function success() {
+
+    }
+    function failed() {
+
+    }
+    function request() {
+
+    }
+
+    return dispatch => {
+
+    }
+}
+
+export function getLogById(id) {
+    function success() {
+
+    }
+    function failed() {
+
+    }
+    function request() {
+
+    }
+
+    return dispatch => {
+
+    }
+}
