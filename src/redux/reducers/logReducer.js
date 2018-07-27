b776a3f6821fbf4eef2c6cd3797a9e564b1f2221
javaxd123 <marcinwarzybok@outlook.com>
2018-07-27 10:53:12 +0200

    add reducer for logs

---
+++ src/redux/reducers/logReducer.js
@@ -0,0 +1,39 @@
+import { LogsConstants } from '../constants'
+
+const initState = {
+
+}
+
+export default function (state = initState, action) {
+    switch (action.type) {
+        case LogsConstants.GET_LOGS_SUCCESS:
+            return {
+                ...state,
+                logs: action.logs,
+                requestLogs: false
+            }
+        case LogsConstants.GET_LOGS_REQUEST:
+            return {
+                ...state,
+                requestLogs: true
+            }
+        case LogsConstants.GET_LOG_SUCCESS:
+            return {
+                ...state,
+                log: action.log,
+                requestLog: false
+            }
+        case LogsConstants.GET_LOG_FAIL:
+            return {
+                ...state,
+                requestLog: false
+            }
+        case LogsConstants.GET_LOG_REQUEST:
+            return {
+                ...state,
+                requestLog: true
+            }
+        default:
+            return state
+    }
+}
