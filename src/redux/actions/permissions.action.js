460c14bd54ef579c1bfb591cf756d0c0cf227c7d
javaxd123 <marcinwarzybok@outlook.com>
2018-07-28 18:43:02 +0200

    add permissions to redux

---
+++ src/redux/actions/permissions.action.js
@@ -0,0 +1,52 @@
+import { PermissionsConstants } from '../constants'
+import { PermissionsService } from '../services'
+import { lostSession } from './app.action'
+
+export function getPermissions() {
+    function success(permissions) {
+        return {
+            type: PermissionsConstants.GET_PERMISSIONS_SUCCESS,
+            permissions
+        }
+    }
+
+    function request() {
+        return {
+            type: PermissionsConstants.GET_PERMISSIONS_REQUEST
+        }
+    }
+
+    return dispatch => {
+        dispatch(request())
+        PermissionsService.getPermissions()
+            .then((permissions) => {
+                dispatch(success(permissions))
+            }).catch(() => {
+                dispatch(lostSession())
+            })
+    }
+}
+
+export function putPermissions(permissions) {
+    function success() {
+        return {
+            type: PermissionsConstants.PUT_PERMISSIONS_SUCCESS
+        }
+    }
+
+    function request() {
+        return {
+            type: PermissionsConstants.PUT_PERMISSIONS_REQUEST
+        }
+    }
+
+    return dispatch => {
+        dispatch(request())
+        PermissionsService.putPermissions(permissions)
+            .then(() => {
+                dispatch(success())
+            }).catch(() => {
+                dispatch(lostSession())
+            })
+    }
+}
