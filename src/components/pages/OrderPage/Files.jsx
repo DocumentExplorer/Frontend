9f0d4bb0cec28b584c0840a59354e8c4a4e36464
javaxd123 <marcinwarzybok@outlook.com>
2018-08-01 18:09:40 +0200

    working correctly

--- src/components/pages/OrderPage/Files.jsx
+++ src/components/pages/OrderPage/Files.jsx
@@ -31,47 +31,50 @@ const File = ({ file, isRequired, download, toggle, toggleDelete, permissions })
     let key = Object.keys(filetered)[0]
     console.log(filetered[key])
     return (
-        filetered[key] === localStorage.getItem('role') ?
-            <Col sm="6" md="4">
-                <Card style={{ marginTop: '40px', marginBottom: '20px', minHeight: '300px' }}>
-                    <CardHeader className="header">
-                        <h4>{file[0].slice(0, -2)}</h4>
+        <Col sm="6" md="4">
+            <Card style={{ marginTop: '40px', marginBottom: '20px', minHeight: '300px' }}>
+                <CardHeader className="header">
+                    <h4>{file[0].slice(0, -2)}</h4>
+                    {
+                        filetered[key] === localStorage.getItem('role')
+                            ? <label className="switch">
+                                <input type="checkbox" />
+                                <span className="slider"></span>
+                            </label>
+                            : ''
+                    }
 
-                        <label className="switch">
-                            <input type="checkbox" />
-                            <span className="slider"></span>
-                        </label>
 
-                    </CardHeader>
-                    <CardBody>
+                </CardHeader>
+                <CardBody>
+                    {
+                        file[1] === '00000000-0000-0000-0000-000000000000'
+                            ? 'Brak pliku'
+                            : 'Plik został już wysłany'
+                    }
+                    <p>
                         {
-                            file[1] === '00000000-0000-0000-0000-000000000000'
-                                ? 'Brak pliku'
-                                : 'Plik został już wysłany'
+                            isRequired[1] == true
+                                ? 'Plik jest wymagany'
+                                : 'Plik nie jest wymagany'
                         }
-                        <p>
-                            {
-                                isRequired[1] == true
-                                    ? 'Plik jest wymagany'
-                                    : 'Plik nie jest wymagany'
-                            }
-                        </p>
-                    </CardBody>
-                    <CardFooter>
-                        {
-                            file[1] === '00000000-0000-0000-0000-000000000000'
-                                ? ''
-                                : <Button onClick={(e) => download(file[1])}>Pobierz</Button>
-                        }
-                        {
-                            file[1] === '00000000-0000-0000-0000-000000000000'
+                    </p>
+                </CardBody>
+                <CardFooter>
+                    {
+                        file[1] === '00000000-0000-0000-0000-000000000000'
+                            ? ''
+                            : <Button onClick={(e) => download(file[1])}>Pobierz</Button>
+                    }
+                    {
+                        filetered[key] === localStorage.getItem('role')
+                            ? file[1] === '00000000-0000-0000-0000-000000000000'
                                 ? <Button color="primary" onClick={(e) => toggle(file[0], isRequired[1])}>Wyślij</Button>
                                 : <Button color="danger" onClick={(e) => toggleDelete(file[1])}>Usuń</Button>
-                        }
-                    </CardFooter>
-                </Card>
-            </Col>
-            : ''
-
+                            : ''
+                    }
+                </CardFooter>
+            </Card>
+        </Col>
     )
 }
