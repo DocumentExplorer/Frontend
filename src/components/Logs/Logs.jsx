2f1a4d314b9b2a2592643d4a160aa0375a3b68a9
javaxd123 <marcinwarzybok@outlook.com>
2018-08-03 13:08:31 +0200

    optimize login

--- src/components/Logs/Logs.jsx
+++ src/components/Logs/Logs.jsx
@@ -2,38 +2,37 @@ import React from 'react'
 import { Table } from 'mdbreact'
 import { withRouter } from 'react-router-dom'
 
-export const Logs = ({ data, changeLocation, ...rest }) => {
-    return (
-        <div style={{ display: 'block', maxHeight: '300px', overflowY: 'auto', marginBottom: '30px' }}>
-            <h3>Logi</h3>
-            <Table className="users-list">
-                <thead >
-                    <tr>
-                        <th>ID</th>
-                        <th className="th-lg">Wydarzenie</th>
-                        <th className="th-lg">Czas</th>
-                        <th className="th-lg">Użytkownik</th>
-                        <th className="th-lg">Właściciel</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        data.map((value, index) => (
-                            <tr key={index} className="user-list-item" onClick={() => changeLocation(value.id)}>
-                                <td>{index}</td>
-                                <td>
-                                    {value.event}
-                                </td>
-                                <td>{value.eventDate}</td>
-                                <td>{value.username}</td>
-                                <td>{value.owner1Name}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </Table>
-        </div>
-    )
-}
+export const Logs = ({ data, changeLocation, ...rest }) => (
+    <div style={{ display: 'block', maxHeight: '300px', overflowY: 'auto', marginBottom: '30px' }}>
+        <h3>Logi</h3>
+        <Table className="users-list">
+            <thead >
+                <tr>
+                    <th>ID</th>
+                    <th className="th-lg">Wydarzenie</th>
+                    <th className="th-lg">Czas</th>
+                    <th className="th-lg">Użytkownik</th>
+                    <th className="th-lg">Właściciel</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data.map((value, index) => (
+                        <tr key={index} className="user-list-item" onClick={() => changeLocation(value.id)}>
+                            <td>{index}</td>
+                            <td>
+                                {value.event}
+                            </td>
+                            <td>{value.eventDate}</td>
+                            <td>{value.username}</td>
+                            <td>{value.owner1Name}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </Table>
+    </div>
+)
+
 
 export default withRouter(Logs)
--- src/components/pages/DashboardPage/InstantWork.jsx
+++ src/components/pages/DashboardPage/InstantWork.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Card, CardBody, Row, Col } from 'mdbreact'
 
 export default ({ lacks, changeLocation }) => (
-
     lacks.count !== 0 ?
         <Row className="custom-row">
             <Card style={{ width: '72%', marginLeft: '20px', background: '#ef7070' }}>
